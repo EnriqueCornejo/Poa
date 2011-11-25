409b147753ec52d387ada396ad3d162b4cdf11ca
Enrique Cornejo <srcornejo@gmail.com>
2011-11-25 10:11:08 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Metaanalisis::Application.config.secret_token = '1a50b10c34954634c053dbc48215cc17cecdc600f221e88dd4deee3ba150c264209bf9148db7256e06cc43f78e9c5cd2793fc2d621a37dfc422dc87562cf3db1'
